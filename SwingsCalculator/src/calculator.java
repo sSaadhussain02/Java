722887f8fb8e3bb747d6190e9b1324527c3c47f4
sSaadhussain02 <134719430+sSaadhussain02@users.noreply.github.com>
2023-08-29 01:15:20 +0500

    Add files via upload

---
+++ SwingsCalculator/src/calculator.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class calculator {
+    private JTextArea textarea2;
+    private JButton button1;
+
+
+        button1.addActionListener(new void ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textarea2.getText();
+                int a;
+                a = Integer.parseInt(textarea2.getText()) ;
+            }
+        });
+
+    public void click()
+    {
+        button1.
+    }
+
+    public
+}
